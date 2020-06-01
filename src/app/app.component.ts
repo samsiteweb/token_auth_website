dcfbab4b9d7c32c2452b9640f1f26c4eeffcd219
Lawal Samuel Olamide <samthedonz@gmail.com>
2020-06-01 05:32:39 +0100

    latest commit before hand over laptop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'activetoken';
+  message: string;
+  isError: boolean;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {}
 }
